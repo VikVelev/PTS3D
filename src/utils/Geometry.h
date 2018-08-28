82b7d9bdb1fe6443aaa0f391cc40020cbff427a7
VikVelev <viktorvelev8@gmail.com>
2018-08-28 03:24:35 +0300

    Refactor, added the possibility to turn off antialiasing

--- src/utils/Geometry.h
+++ src/utils/Geometry.h
@@ -1,18 +1,6 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
 
-#ifndef VECTOR_H
-#include "../main/Vector.cpp"
-#endif
-
-#ifndef COLOR_H
-#include "../main/Color.cpp"
-#endif
-
-#ifndef RAY_H
-#include "../main/Ray.cpp"
-#endif
-
 //Hitable abstraction, so we can use it as an interface later on.
 
 struct hitRecord {
